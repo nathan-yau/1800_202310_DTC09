5ca1314168bbe7ace8513468b2aa0da6d64e64f9
Sung <slee1070@my.bcit.ca>
2023-02-10 00:41:45 -0800

    Add navbar to index.html

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,8 @@
-function sayHello() {
-    
-}
-//sayHello();
+function openNav() {
+    document.getElementById("navbar").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("navbar").style.width = "0";
+  }
+
