dcdd2c8273d393e0d4e2c87c71df79cba350824d
NATHAN YAU <nshyau@gmail.com>
2023-02-24 12:56:43 -0800

    Add login page and link to index.html

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,11 @@
 function openNav() {
-    document.getElementById("navbar").style.width = "250px";
-  }
-  
-  function closeNav() {
-    document.getElementById("navbar").style.width = "0";
-  }
+  document.getElementById("navbar").style.width = "250px";
+}
 
+function closeNav() {
+  document.getElementById("navbar").style.width = "0";
+}
+
+$(".login").click(function () {
+  window.location = "./login.html";
+});
