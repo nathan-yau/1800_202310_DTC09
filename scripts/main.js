0f9a31e82239aa773c7483e9d8fb6fb0bf6f55b5
nathan-yau <nshyau@gmail.com>
2023-02-28 13:27:15 -0800

    Modularize nav and footer for easier design

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,46 @@
-function insertName() {
-    firebase.auth().onAuthStateChanged(user => { //will verify whos logged in
-        // Check if a user is signed in:
+function displayCardsDynamically(collection) {
+    let cardTemplate = document.getElementById("communityPlaceholder");
+
+    db.collection(collection).get()   //the collection called "hikes"
+        .then(allCommunity => {
+            var i = 1;  //Optional: if you want to have a unique ID for each hike
+            allCommunity.forEach(doc => { //iterate thru each doc
+                var area = doc.data().area;
+                var country = doc.data().country;
+                var latitude = doc.data().latitude;
+                var longitude = doc.data().longitude;
+                var place = doc.data().place;
+                var postal_code = doc.data().postal_code;
+                var province = doc.data().province;
+                var region = doc.data().region;
+                // var details = doc.data().details;  // get value of the "details" key
+                var docID = doc.id;
+                let newcard = cardTemplate.content.cloneNode(true);
+                console.log(area, docID)
+
+                //update title and text and image
+                newcard.querySelector('.area').innerHTML = area + " (" + postal_code + ")";
+                newcard.querySelector('.region').innerHTML = region + " (" + province + ", " + country + ")";
+                // newcard.querySelector('.card-text').innerHTML = details;
+                newcard.querySelector('a').href = "eachHike.html?docID=" + docID;
+                document.getElementById(collection + "-go-here").appendChild(newcard);
+
+
+                //i++;   //Optional: iterate variable to serve as unique ID
+            })
+        })
+}
+
+function page_distribution() {
+    firebase.auth().onAuthStateChanged(function (user) {
         if (user) {
-            // Do something for the currently logged-in user here: 
-            console.log(user.uid); //print the uid in the browser console (The hash code of the user)
-            console.log(user.displayName);  //print the user name in the browser console
-            user_Name = user.displayName;
-
-            //method #1:  insert with html only
-            //document.getElementById("name-goes-here").innerText = user_Name;    //using javascript
-            //method #2:  insert using jquery
-            $("#signing").children().remove();
-            $("#signing").html(`<a href="./logout.html">Logout<br><span style="font-size: 20px;">(${user_Name})</span></a>`);
-
+            console.log($('#mainPlaceholder').load('./text/main_after_login.html'));
+            displayCardsDynamically("communities");
         } else {
-            // No user is signed in.
-            $("#main-context").html(`
-                <div class="login-div">
-                    <h1 style="text-align: center;margin-bottom: 5%;">Join the conversation and share your own weather updates</h1>
-                    <h1 style="text-align: center;margin-bottom: 5%;">Log in to LifeLine to get started.</h1>
-                    <button class="login"> LOG IN / SIGN UP</button>
-                </div>`)
+            console.log($('#mainPlaceholder').load('./text/main_before_login.html'));
         }
     });
+
 }
-insertName(); //run the function
+
+page_distribution(); //run the function
--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -2,8 +2,33 @@
 // This function loads the parts of your skeleton 
 // (navbar, footer, and other things) into html doc. 
 //---------------------------------------------------
-function loadSkeleton(){
-    console.log($('#navbarPlaceholder').load('./text/nav.html'));
-    console.log($('#footerPlaceholder').load('./text/footer.html'));
+function loadSkeleton() {
+
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            // User is signed in.
+            // Do something for the user here.
+            console.log($('#navbarPlaceholder').load('./text/nav_after_login.html'));
+            console.log($('#footerPlaceholder').load('./text/footer_after_login.html'));
+        } else {
+            // No user is signed in.
+            console.log($('#navbarPlaceholder').load('./text/nav_before_login.html'));
+            console.log($('#footerPlaceholder').load('./text/footer_before_login.html'));
+        }
+    });
+    
 }
+
+function openNav() {
+    document.getElementById("navbar").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("navbar").style.width = "0";
+}
+
+$("body").on("click", ".login", function () {
+    window.location = "./login.html";
+});
+
 loadSkeleton();  //invoke the function
