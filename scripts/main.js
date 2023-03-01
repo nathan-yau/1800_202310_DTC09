8c4e2457cca194f16fb4f5948d32879e9f21bfe9
nathan-yau <nshyau@gmail.com>
2023-02-28 16:06:37 -0800

    Include logic to default show MetroVancouver data only

--- scripts/main.js
+++ scripts/main.js
@@ -1,34 +1,36 @@
 function displayCardsDynamically(collection) {
     let cardTemplate = document.getElementById("communityPlaceholder");
 
-    db.collection(collection).get()   //the collection called "hikes"
-        .then(allCommunity => {
-            var i = 1;  //Optional: if you want to have a unique ID for each hike
-            allCommunity.forEach(doc => { //iterate thru each doc
-                var area = doc.data().area;
-                var country = doc.data().country;
-                var latitude = doc.data().latitude;
-                var longitude = doc.data().longitude;
-                var place = doc.data().place;
-                var postal_code = doc.data().postal_code;
-                var province = doc.data().province;
-                var region = doc.data().region;
-                // var details = doc.data().details;  // get value of the "details" key
-                var docID = doc.id;
-                let newcard = cardTemplate.content.cloneNode(true);
-                console.log(area, docID)
+    db.collection(collection).get().then(allCommunity => {
+        var i = 1;  //Optional: if you want to have a unique ID for each hike
+        allCommunity.forEach(doc => { //iterate thru each doc
+            var area = doc.data().area;
+            var country = doc.data().country;
+            var latitude = doc.data().latitude;
+            var longitude = doc.data().longitude;
+            var place = doc.data().place;
+            var postal_code = doc.data().postal_code;
+            var province = doc.data().province;
+            var region = doc.data().region;
+            // var details = doc.data().details;  // get value of the "details" key
+            var docID = doc.id;
+            let newcard = cardTemplate.content.cloneNode(true);
 
-                //update title and text and image
-                newcard.querySelector('.area').innerHTML = area + " (" + postal_code + ")";
-                newcard.querySelector('.region').innerHTML = region + " (" + province + ", " + country + ")";
-                // newcard.querySelector('.card-text').innerHTML = details;
-                newcard.querySelector('a').href = "eachHike.html?docID=" + docID;
+            //update title and text and image
+            newcard.querySelector('.area').innerHTML = area + " (" + postal_code + ")";
+            newcard.querySelector('.region').innerHTML = region + " (" + province + ", " + country + ")";
+            // newcard.querySelector('.card-text').innerHTML = details;
+            newcard.querySelector('a').href = "eachHike.html?docID=" + docID;
+            if (region == "Metro Vancouver") {
                 document.getElementById(collection + "-go-here").appendChild(newcard);
-
-
-                //i++;   //Optional: iterate variable to serve as unique ID
-            })
+                i++;
+            }
+            
+            if (i > 10) {
+                throw 'Break';
+            }
         })
+    })
 }
 
 function page_distribution() {
