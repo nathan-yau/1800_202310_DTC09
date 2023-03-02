ffb8ea7ae28435ce17d2f07daaf2aabc3f885695
nathan-yau <nshyau@gmail.com>
2023-03-02 00:35:57 -0800

    Add UI for message posting

--- scripts/eachCommunity.js
+++ scripts/eachCommunity.js
@@ -57,6 +57,15 @@ function displayCommunityPostDynamically(collection) {
         )
 }
 
+let popup = document.getElementById("popup")
+
+function openPopup(){
+    popup.classList.add("open-popup")
+}
+
+function closePopup(){
+    popup.classList.remove("open-popup")
+}
 
 displayCommunityDescriptionDynamically("communities");
 displayCommunityPostDynamically("posts");
