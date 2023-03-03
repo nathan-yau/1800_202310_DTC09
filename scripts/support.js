fd4b482ec7d6b439c7606b564ffda2fb63bcc53d
Reza Hedieloo <reza_hedieloo@yahoo.com>
2023-03-02 21:10:07 -0800

    Changed the support.html Added support.js

---
+++ scripts/support.js
@@ -0,0 +1,40 @@
+
+function faqFunction() {
+  var x = document.getElementById("faq_text");
+  faq_text.hidden = false;
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block ";
+  } 
+}
+
+function tutorialFunction() {
+  var x = document.getElementById("tutorial_text");
+  tutorial_text.hidden = false;
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block ";
+  }
+}
+
+function contactFunction() {
+  var x = document.getElementById("contact_text");
+  contact_text.hidden = false;
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block ";
+  }
+}
+
+function aboutFunction() {
+  var x = document.getElementById("about_text");
+  about_text.hidden = false;
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block ";
+  }
+}
