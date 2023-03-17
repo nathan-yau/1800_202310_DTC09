0d46266db7b7e92a4f4ae072c1bd7ea24e3c40b6
nathan-yau <nshyau@gmail.com>
2023-03-17 15:52:44 -0700

    Add dark mode for background

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -18,7 +18,7 @@ function loadSkeleton() {
             $('#footerPlaceholder').load('./text/footer_before_login.html');
         }
     });
-    
+
 }
 
 function openNav() {
@@ -38,10 +38,27 @@ function logout() {
         // Sign-out successful.
         console.log("logging out user");
         location.replace('./index.html');
-      }).catch((error) => {
+    }).catch((error) => {
         // An error happened.
-      });
+    });
 }
 
+function dark_mode() {
+    if ($(".dark-enable").is(':checked') == true) {
+        $('.sidebar').css('background-color', 'darkslategrey');
+        $('#header').css('background-color', '#CF6679')
+        $('body').attr('style', 'background-color: #121212 !important;transition: all .5s;');
+    }
+    else {
+        $('.sidebar').css('background-color', '#00cbad');
+        $('#header').css('background-color', 'orange')
+        $('body').attr('style', 'background-color: #f8f9fa!important;transition: all .5s;');
+
+    }
+}
 
 loadSkeleton();  //invoke the function
+
+// $(document).ready(function() {
+//     .is(':checked')
+// })
