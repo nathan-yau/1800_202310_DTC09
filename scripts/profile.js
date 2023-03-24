fde7bc7ccc5d437a8adc7046a36e517ebce4728f
Sung <slee1070@my.bcit.ca>
2023-03-24 08:13:20 -0700

    Implement a profile photo upload function

--- scripts/profile.js
+++ scripts/profile.js
@@ -10,19 +10,28 @@ function populateUserInfo() {
                 .then(userDoc => {
                     //get the data fields of the user
                     var userName = userDoc.data().name;
-                    var UserPC = userDoc.data().postalcode;
+                    var userPC = userDoc.data().postalcode;
                     var userCity = userDoc.data().city;
+                    var picUrl = userDoc.data().profile_picture;
 
                     //if the data fields are not empty, then write them in to the form.
                     if (userName != null) {
                         document.getElementById("nameInput").value = userName;
                     }
-                    if (UserPC != null) {
+                    if (userPC != null) {
                         document.getElementById("postalCodeInput").value = userPC;
                     }
                     if (userCity != null) {
                         document.getElementById("cityInput").value = userCity;
                     }
+                    if (picUrl != null){
+                        console.log(picUrl);
+                        // use this line if "mypicdiv" is a "div"
+                        //$("#mypicdiv").append("<img src='" + picUrl + "'>")
+                        $("#mypic-goes-here").attr("src", picUrl);
+                    }
+                    else
+                    console.log("picURL is null");
                 })
         } else {
             // No user is signed in.
@@ -31,26 +40,66 @@ function populateUserInfo() {
     });
 }
 
+
 function editUserInfo() {
     //Enable the form fields
     document.getElementById('personalInfoFields').disabled = false;
 }
 
-function saveUserInfo() {
-    var userName = document.getElementById("nameInput").value;
-    var userPC = document.getElementById("postalCodeInput").value;
-    var userCity = document.getElementById("cityInput").value;
-    console.log(userName, userPC, userCity);
-    currentUser.update({
-        name: userName,
-        postalcode: userPC,
-        city: userCity
+var ImageFile;      //global variable to store the File Object reference
+
+function chooseFileListener(){
+    const fileInput = document.getElementById("mypic-input");   // pointer #1
+    const image = document.getElementById("mypic-goes-here");   // pointer #2
+
+    //attach listener to input file
+    //when this file changes, do something
+    fileInput.addEventListener('change', function(e){
+
+        //the change event returns a file "e.target.files[0]"
+	      ImageFile = e.target.files[0];
+        var blob = URL.createObjectURL(ImageFile);
+
+        //change the DOM img element source to point to this file
+        image.src = blob;    //assign the "src" property of the "img" tag
     })
-        .then(() => {
-            console.log("Document successfully updated!");
-        })
-    document.getElementById('personalInfoFields').disabled = true;
+}
+chooseFileListener();
+
+function saveUserInfo() {
+    firebase.auth().onAuthStateChanged(function (user) {
+        var storageRef = storage.ref("images/" + user.uid + ".jpg");
+
+        //Asynch call to put File Object (global variable ImageFile) onto Cloud
+        storageRef.put(ImageFile)
+            .then(function () {
+                console.log('Uploaded to Cloud Storage.');
+
+                //Asynch call to get URL from Cloud
+                storageRef.getDownloadURL()
+                    .then(function (url) { // Get "url" of the uploaded file
+                        console.log("Got the download URL.");
+						//get values from the from
+                        var userName = document.getElementById("nameInput").value;
+                        var userPC = document.getElementById("postalCodeInput").value;
+                        var userCity = document.getElementById("cityInput").value;
+                        console.log(userName, userPC, userCity);
+
+                        //Asynch call to save the form fields into Firestore.
+                        currentUser.update({
+                                name: userName,
+                                postalcode: userPC,
+                                city: userCity,
+                                profile_picture: url // Save the URL into users collection
+                            })
+                            .then(function () {
+                                console.log('Added Profile Pic URL to Firestore.');
+                                console.log('Saved user profile info');
+                                document.getElementById('personalInfoFields').disabled = true;
+                            })
+                    })
+            })
+    })
 }
 
-//call the function to run it 
 populateUserInfo();
