50738badcdab096b475207efd57f9eccd196a2a3
Jason Liu <tliu139@my.bcit.ca>
2023-03-29 12:04:25 -0700

    Update support.html JavaScript and CSS

--- scripts/support.js
+++ scripts/support.js
@@ -1,40 +1,14 @@
 
-function faqFunction() {
-  var x = document.getElementById("faq_text");
-  faq_text.hidden = false;
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block ";
-  } 
-}
-
-function tutorialFunction() {
-  var x = document.getElementById("tutorial_text");
-  tutorial_text.hidden = false;
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block ";
-  }
-}
-
-function contactFunction() {
-  var x = document.getElementById("contact_text");
-  contact_text.hidden = false;
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block ";
-  }
-}
-
-function aboutFunction() {
-  var x = document.getElementById("about_text");
-  about_text.hidden = false;
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block ";
-  }
+var faq = document.getElementsByClassName("faq-page");
+var i;
+for (i = 0; i < faq.length; i++) {
+    faq[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var body = this.nextElementSibling;
+        if (body.style.display === "block") {
+            body.style.display = "none";
+        } else {
+            body.style.display = "block";
+        }
+    });
 }
